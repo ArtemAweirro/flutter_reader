4bc20eab90e39603c829895f02c56aafce9f09b7
ArtemAweirro <sixvart05@bk.ru>
2026-06-22 16:19:25 +0600

    feat: implement BookRepository interface for book management

---
+++ lib/features/book_list/domain/repositories/book_repository.dart
@@ -0,0 +1,32 @@
+import 'package:flutter_reader/core/error/failures.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_reader/features/book_list/domain/entities/book.dart';
+
+abstract class BookRepository {
+  /// Получить поток всех книг
+  Stream<Either<Failure, List<Book>>> watchAllBooks();
+
+  /// Получить поток только избранных книг
+  Stream<Either<Failure, List<Book>>> watchFavoriteBooks();
+
+  /// Получить поток только прочитанных книг
+  Stream<Either<Failure, List<Book>>> watchReadBooks();
+
+  /// Получить книгу по ID
+  Future<Either<Failure, Book?>> getBookById(int id);
+
+  /// Добавить новую книгу
+  Future<Either<Failure, int>> addBook(Book book);
+
+  /// Обновить книгу
+  Future<Either<Failure, bool>> updateBook(Book book);
+
+  /// Удалить книгу по ID
+  Future<Either<Failure, void>> deleteBook(int id);
+
+  /// Переключить статус "избранное"
+  Future<Either<Failure, void>> toggleFavorite(int id, bool value);
+
+  /// Переключить статус "прочитано"
+  Future<Either<Failure, void>> toggleRead(int id, bool value);
+}
